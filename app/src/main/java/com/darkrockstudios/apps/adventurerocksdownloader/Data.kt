d2851016be225113309ef98d6f9487e8e707549e
Adam Brown <adamwbrown@gmail.com>
2020-11-15 19:04:43 -0800

    - Added intro tutorial - Added link to website

---
+++ app/src/main/java/com/darkrockstudios/apps/adventurerocksdownloader/Data.kt
@@ -0,0 +1,6 @@
+package com.darkrockstudios.apps.adventurerocksdownloader
+
+object Data
+{
+	const val KEY_INTRO_SEEN = "intro_seen"
+}
--- app/src/main/java/com/darkrockstudios/apps/adventurerocksdownloader/PhotosActivity.kt
+++ app/src/main/java/com/darkrockstudios/apps/adventurerocksdownloader/PhotosActivity.kt
@@ -1,27 +1,24 @@
 package com.darkrockstudios.apps.adventurerocksdownloader
 
+import android.content.Intent
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
-import com.beust.klaxon.Klaxon
-import com.darkrockstudios.apps.adventurerocksdownloader.database.AppDatabase
-import com.darkrockstudios.apps.adventurerocksdownloader.database.Photo360
-import com.log4k.d
-import com.log4k.e
-import com.log4k.i
-import com.log4k.w
 import kotlinx.android.synthetic.main.activity_photos.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import org.koin.android.ext.android.inject
 
 class PhotosActivity: AppCompatActivity()
 {
+	private val storage by lazy { PreferenceManager.getDefaultSharedPreferences(this) }
+
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_photos)
 		setSupportActionBar(toolbar)
+
+		if(!storage.getBoolean(Data.KEY_INTRO_SEEN, false))
+		{
+			startActivity(Intent(this, IntroActivity::class.java))
+		}
 	}
 }
